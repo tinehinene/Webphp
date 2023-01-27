b125c3c2bc5018345ca3af21695f00101161f644
tinehinene <123732964+tinehinene@users.noreply.github.com>
2023-01-27 13:33:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php phpinfo();?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Exemple PHP</title>
+</head>
+<body>
+    <h1>Bonjour vous</h1>
+    <p>Nous sommes le <?php echo date("d/m/Y") ?></p>
+</body>
+</html>
