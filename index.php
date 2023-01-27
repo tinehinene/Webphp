ba7de28cde84d25e7bd2c6ec6459ed6aab8b5e63
tinehinene <123732964+tinehinene@users.noreply.github.com>
2023-01-27 13:35:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>Exemple PHP</title>
 </head>
 <body>
-    <h1>Bonjour vous</h1>
+    <h1>Bonjour et bienvenue sur mon site</h1>
     <p>Nous sommes le <?php echo date("d/m/Y") ?></p>
 </body>
 </html>
